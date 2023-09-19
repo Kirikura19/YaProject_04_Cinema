cd304813fbdb38eabc804e9d242dca0deed6cf14
Kirikura <kirikura.w@yanex.ru>
2023-09-19 19:24:34 +0300

    Initial commit

---
+++ src/main/java/ru/kirikura/yaproject_05_cinema/YaProject05CinemaApplication.java
@@ -0,0 +1,13 @@
+package ru.kirikura.yaproject_05_cinema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YaProject05CinemaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YaProject05CinemaApplication.class, args);
+    }
+
+}
