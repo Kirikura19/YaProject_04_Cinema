a82691c414a181f6ef1e81ca32323b361b411b8f
Kirikura <kirikura.w@yanex.ru>
2023-09-19 19:35:04 +0300

    Added User and Film entity. Added User and Film controllers. Added validation ang logs.

---
+++ src/main/java/ru/kirikura/yaproject_05_cinema/exceptions/ValidationException.java
@@ -0,0 +1,7 @@
+package ru.kirikura.yaproject_05_cinema.exceptions;
+
+public class ValidationException extends Throwable{
+    public ValidationException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/ru/kirikura/yaproject_05_cinema/model/User.java
@@ -0,0 +1,22 @@
+package ru.kirikura.yaproject_05_cinema.model;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+@Data
+public class User {
+    private static int identificator = 0;
+    private int id = setId();
+    @Email
+    private String email;
+    @NotBlank
+    private String login;
+    private String name;
+    @NotBlank
+    private String birthday;
+    public int setId() {
+        return ++identificator;
+    }
+}
+
+
